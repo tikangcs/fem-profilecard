e5717e51e6cd9cad195c872e17c3d7cbc7e4def9
Tim Kang <tikang.cs@gmail.com>
2021-06-24 22:15:44 -0400

    #1 - File setup with react webpack babel

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App() {
+  return <div>Victor Crest 26 London 80K Followers 803K Likes 1.4K Photos</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
